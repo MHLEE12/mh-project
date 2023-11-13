592c30522676d38d42fb75c3ec9d83fcb6d3d72f
MHLEE12 <coolmh12@gmail.com>
2023-11-13 23:07:48 +0900

    #16 - 게시판 리스트 view 기본 틀 작성
    게시판 리스트 view페이지 생성 및 게시판 리스트 테스트 통과하게 함.

--- src/main/java/com/mh/project/controller/PostController.java
+++ src/main/java/com/mh/project/controller/PostController.java
@@ -1,9 +1,19 @@
 package com.mh.project.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @RequestMapping("/posts")
 @Controller
 public class PostController {
+
+    @GetMapping
+    public String posts(ModelMap map) {
+        map.addAttribute("posts", List.of());
+        return "posts/index";
+    }
 }
--- src/test/java/com/mh/project/controller/PostControllerTest.java
+++ src/test/java/com/mh/project/controller/PostControllerTest.java
@@ -21,7 +21,6 @@ public class PostControllerTest {
         this.mvc = mvc;
     }
 
-    @Disabled("구현 중")
     @DisplayName("[view][GET] 게시글 리스트 페이지 - 정상 호출")
     @Test
     void show_postListPage() throws Exception {
@@ -30,7 +29,7 @@ public class PostControllerTest {
         // When & Then
         mvc.perform(get("/posts"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("posts/index"))
                 .andExpect(model().attributeExists("posts"));
     }
@@ -44,7 +43,7 @@ public class PostControllerTest {
         // When & Then
         mvc.perform(get("/posts/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("posts/detail"))
                 .andExpect(model().attributeExists("post"))
                 .andExpect(model().attributeExists("comments"));
@@ -59,7 +58,7 @@ public class PostControllerTest {
         // When & Then
         mvc.perform(get("/posts/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("posts/search-hashtag"));
     }
 }
