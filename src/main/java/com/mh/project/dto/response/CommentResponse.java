f409607c85d3df75c62322e21da04ea6b7540ac6
MHLEE12 <coolmh12@gmail.com>
2023-11-17 20:54:02 +0900

    #18 - userAccount 추가로 인한 변경 및 필요한 DTO들 추가
    수정 및 필요에 따라 DTO를 추가로 생성하였다.

---
+++ src/main/java/com/mh/project/dto/response/CommentResponse.java
@@ -0,0 +1,34 @@
+package com.mh.project.dto.response;
+
+import com.mh.project.dto.CommentDTO;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public record CommentResponse(
+        Long id,
+        String content,
+        LocalDateTime creDate,
+        String email,
+        String nickname
+) implements Serializable {
+
+    public static CommentResponse of(Long id, String content, LocalDateTime creDate, String email, String nickname) {
+        return new CommentResponse(id, content, creDate, email, nickname);
+    }
+
+    public static CommentResponse from(CommentDTO dto) {
+        String nickname = dto.userAccountDTO().nickname();
+        if (nickname == null || nickname.isBlank()) {
+            nickname = dto.userAccountDTO().userId();
+        }
+
+        return new CommentResponse(
+                dto.id(),
+                dto.content(),
+                dto.creDate(),
+                dto.userAccountDTO().email(),
+                nickname
+        );
+    }
+}
