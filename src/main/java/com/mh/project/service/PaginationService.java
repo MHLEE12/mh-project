d62386d7917b037ddcbfc3b72e80d86e44073473
MHLEE12 <coolmh12@gmail.com>
2023-11-20 11:35:00 +0900

    #23 - 게시글 상세페이지에서 페이징 처리
    prev 버튼 구현 완료.
    next는 마지막 게시글까지 가면 disabled 처리되어야 하는데 이 처리를 하지 못함. 추후 수정 예정

--- src/main/java/com/mh/project/service/PaginationService.java
+++ src/main/java/com/mh/project/service/PaginationService.java
@@ -9,14 +9,22 @@ import java.util.stream.IntStream;
 public class PaginationService {
 
     private static final int BAR_LENGTH = 5;
+    private int totalPostSize = 0;
 
     public List<Integer> getPaginationBarNumbers(int currentPageNum, int totalPages) {
         int startNum = Math.max(currentPageNum - (BAR_LENGTH / 2), 0);
         int endNum = Math.min(startNum + BAR_LENGTH, totalPages);
+
+        this.totalPostSize = totalPages;
+
         return IntStream.range(startNum, endNum).boxed().toList();
     }
 
     public int currentBarLength() {
         return BAR_LENGTH;
+    }
+
+    public int currentPostSize() {
+        return this.totalPostSize;
     }
 }
